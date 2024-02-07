dee1efe237b6932cccbf00c4cb9e1d2795002c15
Robert Tabacaru <robert.tabacaru@seedblink.com>
2024-02-07 10:21:45 +0200

    add axios request

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const jwksClient = require('jwks-rsa')
+const axios = require('axios');
 
 const client = jwksClient({
     jwksUri: 'http://localhost:8080/realms/nimity/protocol/openid-connect/certs'
 });
 
 (async () => {
-    const keys = await client.getSigningKeys();
-    console.log(keys);
+    const response = await axios.get('http://localhost:8080/realms/nimity/protocol/openid-connect/certs');
+
+    console.log(response.status);
+    console.log(response.data);
+    // const keys = await client.getSigningKeys();
+    // console.log(keys);
 })()
 
